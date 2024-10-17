ac58be81374b81aee0a081841bc3916a5ed76957
Gurupriyan j <gurupriyanj007@gmail.com>
2024-10-18 02:48:33 +0530

    update server tsc

--- dist/controllers/admin/authController.js
+++ dist/controllers/admin/authController.js
@@ -15,36 +15,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.adminLogin = void 0;
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
-const constants_1 = require("../../config/constants");
+// import { generateToken } from "../../config/constants";
 const admin_1 = __importDefault(require("../../models/admin"));
 // POST ||  Login
 exports.adminLogin = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(400);
-        throw new Error("Please enter all the fields");
-    }
-    const admin = yield admin_1.default.findOne({
-        email,
-        isDeleted: false,
-    });
-    if (!admin) {
-        res.status(400);
-        throw new Error("Your email has no account");
-    }
-    const isMatch = yield bcryptjs_1.default.compare(password, admin.password);
-    if (!isMatch) {
-        res.status(400);
-        throw new Error("Incorrect Password!");
-    }
-    let adminInfo = {
-        email: admin === null || admin === void 0 ? void 0 : admin.email,
-        id: admin === null || admin === void 0 ? void 0 : admin._id,
-        token: (0, constants_1.generateToken)(admin === null || admin === void 0 ? void 0 : admin._id, "7d"),
-    };
-    res.status(200).json({
-        success: true,
-        admin: adminInfo,
-        msg: "Login successfully",
-    });
+    const { email, password, name } = req.body;
+    const salt = yield bcryptjs_1.default.genSalt(10);
+    const hashedPassword = yield bcryptjs_1.default.hash(password, salt);
+    const admin = yield admin_1.default.create(Object.assign(Object.assign({}, req.body), { password: hashedPassword }));
+    // if (!email || !password) {
+    //   res.status(400);
+    //   throw new Error("Please enter all the fields");
+    // }
+    // const admin = await Admin.findOne({
+    //   email,
+    //   isDeleted: false,
+    // });
+    // if (!admin) {
+    //   res.status(400);
+    //   throw new Error("Your email has no account");
+    // }
+    // const isMatch = await bcrypt.compare(password, admin.password);
+    // if (!isMatch) {
+    //   res.status(400);
+    //   throw new Error("Incorrect Password!");
+    // }
+    // let adminInfo = {
+    //   email: admin?.email,
+    //   id: admin?._id,
+    //   token: generateToken(admin?._id, "7d"),
+    // };
+    // res.status(200).json({
+    //   success: true,
+    //   admin: adminInfo,
+    //   msg: "Login successfully",
+    // });
 }));
--- src/controllers/admin/authController.ts
+++ src/controllers/admin/authController.ts
@@ -2,40 +2,50 @@ import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 
-import { generateToken } from "../../config/constants";
+// import { generateToken } from "../../config/constants";
 import Admin from "../../models/admin";
 
 // POST ||  Login
 export const adminLogin = asyncHandler(async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  const { email, password,name } = req.body;
+    const salt: string = await bcrypt.genSalt(10);
 
-  if (!email || !password) {
-    res.status(400);
-    throw new Error("Please enter all the fields");
-  }
-  const admin = await Admin.findOne({
-    email,
-    isDeleted: false,
+  const hashedPassword=  await bcrypt.hash(password, salt);
+
+
+
+  const admin = await Admin.create({
+    ...req.body,
+    password: hashedPassword,
   });
 
-  if (!admin) {
-    res.status(400);
-    throw new Error("Your email has no account");
-  }
-  const isMatch = await bcrypt.compare(password, admin.password);
+  // if (!email || !password) {
+  //   res.status(400);
+  //   throw new Error("Please enter all the fields");
+  // }
+  // const admin = await Admin.findOne({
+  //   email,
+  //   isDeleted: false,
+  // });
 
-  if (!isMatch) {
-    res.status(400);
-    throw new Error("Incorrect Password!");
-  }
-  let adminInfo = {
-    email: admin?.email,
-    id: admin?._id,
-    token: generateToken(admin?._id, "7d"),
-  };
-  res.status(200).json({
-    success: true,
-    admin: adminInfo,
-    msg: "Login successfully",
-  });
+  // if (!admin) {
+  //   res.status(400);
+  //   throw new Error("Your email has no account");
+  // }
+  // const isMatch = await bcrypt.compare(password, admin.password);
+
+  // if (!isMatch) {
+  //   res.status(400);
+  //   throw new Error("Incorrect Password!");
+  // }
+  // let adminInfo = {
+  //   email: admin?.email,
+  //   id: admin?._id,
+  //   token: generateToken(admin?._id, "7d"),
+  // };
+  // res.status(200).json({
+  //   success: true,
+  //   admin: adminInfo,
+  //   msg: "Login successfully",
+  // });
 });
