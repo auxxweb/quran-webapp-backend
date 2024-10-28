acceb8564568e7ddb860c31b8c2d51ea9e698b39
sahad <msahadpp@gmail.com>
2024-10-28 13:27:58 +0530

    updated block and unblock route

--- dist/controllers/admin/judgeController.js
+++ dist/controllers/admin/judgeController.js
@@ -25,7 +25,7 @@ exports.uploadJudgeDetails = (0, express_async_handler_1.default)((req, res) => 
     const imageUrl = image && ((_a = image[0]) === null || _a === void 0 ? void 0 : _a.location);
     if (!name || !email || !phone || !address || !gender || !zone) {
         res.status(400);
-        throw new Error('Please enter all the fields');
+        throw new Error("Please enter all the fields");
     }
     const existJudge = yield judge_1.default.findOne({
         email,
@@ -45,7 +45,7 @@ exports.uploadJudgeDetails = (0, express_async_handler_1.default)((req, res) => 
         res.status(400);
         throw new Error(`A live competition is happening for a participant; can't add a judge.`);
     }
-    if (isMain === true || isMain === 'true') {
+    if (isMain === true || isMain === "true") {
         const mainJudge = yield judge_1.default.findOne({
             zone: new mongoose_1.default.Types.ObjectId(String(zone)),
             isMain: true,
@@ -56,26 +56,26 @@ exports.uploadJudgeDetails = (0, express_async_handler_1.default)((req, res) => 
             throw new Error(`A main judge already exists in this zone`);
         }
     }
-    const plainPassword = crypto_1.default.randomBytes(4).toString('hex').slice(0, 8);
+    const plainPassword = crypto_1.default.randomBytes(4).toString("hex").slice(0, 8);
     const judge = yield judge_1.default.create(Object.assign(Object.assign({}, req.body), { password: plainPassword, image: imageUrl && imageUrl }));
     if (!judge) {
         res.status(400);
-        throw new Error('Judge upload failed');
+        throw new Error("Judge upload failed");
     }
     res.status(201).json({
         success: true,
-        msg: 'Judge details successfully uploaded',
+        msg: "Judge details successfully uploaded",
     });
 }));
 // PATCH || update Judge details
 exports.updateJudgeDetails = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
+    var _b;
     const { judgeId, email, isMain, zone } = req.body;
     const { image } = req.files || {};
-    const imageUrl = image && ((_a = image[0]) === null || _a === void 0 ? void 0 : _a.location);
+    const imageUrl = image && ((_b = image[0]) === null || _b === void 0 ? void 0 : _b.location);
     if (!judgeId) {
         res.status(400);
-        throw new Error('Judge Id  not found');
+        throw new Error("Judge Id  not found");
     }
     if (email) {
         const judge = yield judge_1.default.findOne({
@@ -84,7 +84,7 @@ exports.updateJudgeDetails = (0, express_async_handler_1.default)((req, res) => 
         });
         if (!judge) {
             res.status(404);
-            throw new Error('Judge not found');
+            throw new Error("Judge not found");
         }
         if (judge.email !== email) {
             const existJudge = yield judge_1.default.findOne({
@@ -93,11 +93,11 @@ exports.updateJudgeDetails = (0, express_async_handler_1.default)((req, res) => 
             });
             if (existJudge) {
                 res.status(400);
-                throw new Error('This email already used');
+                throw new Error("This email already used");
             }
         }
     }
-    if ((isMain === true || isMain === 'true') && zone) {
+    if ((isMain === true || isMain === "true") && zone) {
         const isLiveCompetition = yield result_1.default.findOne({
             isDeleted: false,
             isCompleted: false,
@@ -109,17 +109,17 @@ exports.updateJudgeDetails = (0, express_async_handler_1.default)((req, res) => 
             throw new Error(`A live competition is happening for a judge; can't change.`);
         }
     }
-    if ((isMain === true || isMain === 'true') && zone) {
+    if ((isMain === true || isMain === "true") && zone) {
         yield judge_1.default.findOneAndUpdate({ isMain: true, isDeleted: false, _id: { $ne: judgeId } }, { isMain: false }, { new: true });
     }
     const updatedJudge = yield judge_1.default.findOneAndUpdate({ _id: judgeId, isDeleted: false }, Object.assign(Object.assign({}, req.body), { image: imageUrl && imageUrl }), { new: true });
     if (!updatedJudge) {
         res.status(400);
-        throw new Error('Judge not updated');
+        throw new Error("Judge not updated");
     }
     res.status(200).json({
         success: true,
-        msg: 'Judge details successfully updated',
+        msg: "Judge details successfully updated",
     });
 }));
 // DELETE ||  delete judge details
@@ -127,7 +127,7 @@ exports.deletejudgeDetails = (0, express_async_handler_1.default)((req, res) => 
     const { judgeId } = req.query;
     if (!judgeId) {
         res.status(400);
-        throw new Error('judgeId not found');
+        throw new Error("judgeId not found");
     }
     const existJudge = yield judge_1.default.findOne({
         _id: new mongoose_1.default.Types.ObjectId(String(judgeId)),
@@ -144,14 +144,14 @@ exports.deletejudgeDetails = (0, express_async_handler_1.default)((req, res) => 
     });
     if (isInLiveCompetition) {
         res.status(400);
-        throw new Error('The judge is already in a live competition');
+        throw new Error("The judge is already in a live competition");
     }
     const judge = yield judge_1.default.findByIdAndUpdate({ _id: judgeId }, {
         isDeleted: true,
     }, { new: true });
     if (!judge) {
         res.status(400);
-        throw new Error('Deletion failed');
+        throw new Error("Deletion failed");
     }
     res.status(200).json({
         success: true,
@@ -162,23 +162,23 @@ exports.deletejudgeDetails = (0, express_async_handler_1.default)((req, res) => 
 exports.getJudgeDetails = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
-    const sortBy = req.query.sortBy || 'createdAt';
-    const sortOrder = req.query.sortOrder === 'asc' ? 1 : -1;
-    const searchData = req.query.search || '';
-    const zones = req.query.zones || '';
-    const status = req.query.status || '';
+    const sortBy = req.query.sortBy || "createdAt";
+    const sortOrder = req.query.sortOrder === "asc" ? 1 : -1;
+    const searchData = req.query.search || "";
+    const zones = req.query.zones || "";
+    const status = req.query.status || "";
     const query = { isDeleted: false };
-    if (searchData !== '') {
-        query.name = { $regex: new RegExp(`^${searchData}.*`, 'i') };
+    if (searchData !== "") {
+        query.name = { $regex: new RegExp(`^${searchData}.*`, "i") };
     }
-    if (zones !== '') {
+    if (zones !== "") {
         query.zone = { $in: zones };
     }
-    if (status !== '') {
+    if (status !== "") {
         query.isBlocked = status;
     }
     const judge = yield judge_1.default.find(query)
-        .populate('zone')
+        .populate("zone")
         .sort({ [sortBy]: sortOrder })
         .skip((page - 1) * limit)
         .limit(limit);
@@ -188,7 +188,7 @@ exports.getJudgeDetails = (0, express_async_handler_1.default)((req, res) => __a
         judge: judge || [],
         currentPage: page,
         totalPages: Math.ceil(totalDocuments / limit),
-        msg: 'Judge details successfully retrieved',
+        msg: "Judge details successfully retrieved",
     });
 }));
 // PATCH ||  block or unblock judge details
@@ -196,22 +196,36 @@ exports.blockOrUnblock = (0, express_async_handler_1.default)((req, res) => __aw
     const { judgeId } = req.query;
     if (!judgeId) {
         res.status(400);
-        throw new Error('judgeId not found');
+        throw new Error("judgeId not found");
     }
     const judge = yield judge_1.default.findOne({ _id: judgeId }, {
         isBlocked: 1,
     });
     if (!judge) {
         res.status(400);
-        throw new Error('Updation failed');
+        throw new Error("Updation failed");
     }
-    let msg = '';
+    let msg = "";
+    const isInLiveCompetition = yield result_1.default.findOne({
+        zone: new mongoose_1.default.Types.ObjectId(String(judge === null || judge === void 0 ? void 0 : judge.zone)),
+        isCompleted: false,
+        isDeleted: false,
+    });
     if ((judge === null || judge === void 0 ? void 0 : judge.isBlocked) === true) {
+        if (isInLiveCompetition) {
+            res.status(400);
+            throw new Error("This judge's zones already have active competitions, so unblocking is not possible at this time.");
+        }
         msg = `${judge === null || judge === void 0 ? void 0 : judge.name} successfully unblocked`;
         judge.isBlocked = false;
     }
     else {
-        msg = `${judge === null || judge === void 0 ? void 0 : judge.name} successfully blocked`;
+        if (isInLiveCompetition) {
+            msg = `You blocked a judge with an active competition, so unblocking isn't possible until this competition is completed`;
+        }
+        else {
+            msg = `${judge === null || judge === void 0 ? void 0 : judge.name} successfully blocked`;
+        }
         judge.isBlocked = true;
     }
     yield judge.save();
@@ -225,14 +239,14 @@ exports.updatePassword = (0, express_async_handler_1.default)((req, res) => __aw
     const { judgeId, password } = req.query;
     if (!judgeId) {
         res.status(400);
-        throw new Error('judgeId not found');
+        throw new Error("judgeId not found");
     }
     const judge = yield judge_1.default.findByIdAndUpdate({ _id: judgeId }, {
         password: password,
     }, { new: true });
     if (!judge) {
         res.status(400);
-        throw new Error('Password updation failed');
+        throw new Error("Password updation failed");
     }
     res.status(200).json({
         success: true,
@@ -244,19 +258,19 @@ exports.getSingleJudgeDetails = (0, express_async_handler_1.default)((req, res) 
     const { judgeId } = req.params;
     if (!judgeId) {
         res.status(400);
-        throw new Error('judgeId is required');
+        throw new Error("judgeId is required");
     }
     const judge = yield judge_1.default.findOne({
         _id: judgeId,
         isDeleted: false,
-    }).populate('zone');
+    }).populate("zone");
     if (!judge) {
         res.status(400);
-        throw new Error('Judge not found');
+        throw new Error("Judge not found");
     }
     res.status(200).json({
         success: true,
         judge: judge,
-        msg: 'Judge details successfully retrieved',
+        msg: "Judge details successfully retrieved",
     });
 }));
